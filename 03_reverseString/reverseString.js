5056d8d91e93901ab29920acba85f004cf27224c
nanoo-n <88988610+nanoo-n@users.noreply.github.com>
2024-08-24 18:21:01 -0300

    complete js exercise 3 with own solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    //TAKE string
+    let letters = [];
+    let reversedWord = "";
+    //for each letter in the string unshift into array
+    for (let i = 0; i < word.length; i++) {
+        letters.unshift(word[i]);
+    }
+    //console.log(letters.join(""));
+    reversedWord = letters.join("");
+    //return the new string
+    return reversedWord;
 };
 
 // Do not edit below this line
