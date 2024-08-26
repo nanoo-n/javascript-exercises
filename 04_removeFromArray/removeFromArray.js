cec8c40393db51d4d78b8f21c591717c445be2f5
nanoo-n <88988610+nanoo-n@users.noreply.github.com>
2024-08-26 14:55:54 -0300

    exercise 4 with test 1 passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+
+const removeFromArray = function(array, removal) {
+    //CONVERT removal to array
+    let newArray = array.filter((word) => word !== removal);
+    return newArray;
 };
+
+/*
+let testArr = [1, 2, 3, 4, 3, 7, 9, 3, 12];
+let toRemove = "3, 5";
+let toRemove1 = toRemove.split();
+let newArray = testArr.filter((word) => !toRemove1.includes(word));
+console.log("3::: ", newArray);
+*/
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
