7a061bfc932b92a7153165f64c487db18ad98976
nanoo-n <88988610+nanoo-n@users.noreply.github.com>
2024-08-27 12:23:30 -0300

    complete exercise 5 with own (long) answer

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (typeof (start && end) === 'number' && start >= 0 && end >= 0 && Number.isInteger(start) && Number.isInteger(end)) {
+        if (start < end) {
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        }
+        } else if (end < start) {
+            for (let i = end; i <= start; i++) {
+                sum += i;
+            }
+        } 
+        return sum;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
