066a7d983aef0a8ee13e474c585ee0816bd9bcb7
nanoo-n <88988610+nanoo-n@users.noreply.github.com>
2024-08-27 16:57:52 -0300

    adjust exercise 5 with more proper if statements

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,19 +1,18 @@
 const sumAll = function(start, end) {
     let sum = 0;
-    if (typeof (start && end) === 'number' && start >= 0 && end >= 0 && Number.isInteger(start) && Number.isInteger(end)) {
-        if (start < end) {
+    if (start < 0 || end < 0) return "ERROR";
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+
+    if (start < end) {
         for (let i = start; i <= end; i++) {
             sum += i;
         }
-        } else if (end < start) {
-            for (let i = end; i <= start; i++) {
-                sum += i;
-            }
-        } 
-        return sum;
-    } else {
-        return 'ERROR';
-    }
+    } else if (end < start) {
+        for (let i = end; i <= start; i++) {
+        sum += i;
+        }
+    } 
+    return sum;
 };
 
 // Do not edit below this line
