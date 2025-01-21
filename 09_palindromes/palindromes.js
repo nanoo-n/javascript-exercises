1303c4f031c1f8c47ae3e6cf81229a01d38e70f4
nanoo-n <88988610+nanoo-n@users.noreply.github.com>
2025-01-21 11:36:28 -0300

    complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (palindrome) {
+    //Make array and remove spaces and punctuation
+    let normalArray = [...palindrome.replace(/[^\w\']|_/g, "").toLowerCase()];
+    let normalString = normalArray.join("");
+    console.log(normalString);
+    //Create copy & flip
+    let invertedString = normalArray.reverse().join("");
+    console.log(invertedString);
+    
+    return normalString === invertedString;
 };
 
 // Do not edit below this line
