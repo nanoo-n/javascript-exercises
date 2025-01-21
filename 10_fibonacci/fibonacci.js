81d1d68d26a7970071f1441247ce73c451799b0d
nanoo-n <88988610+nanoo-n@users.noreply.github.com>
2025-01-21 11:34:38 -0300

    complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
 
+    let count
+    if (typeof position !== 'number') {
+        count = parseInt(position)
+    } else {
+        count = position
+    }
+
+    if (count < 0) return "OOPS";
+    if (count === 0) return 0;
+    let positionZero = 0;
+    let positionOne = 1;
+
+    for(let i = 1; i < count; i++) {
+        let positionCurrent = positionZero + positionOne;
+        positionZero = positionOne;
+        positionOne = positionCurrent;
+    }
+
+    return positionOne;
+    //FinP (example 4) => 1,1,2,[3]
 };
 
 // Do not edit below this line
