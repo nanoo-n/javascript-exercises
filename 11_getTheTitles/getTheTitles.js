2a015b815a37106b0d8969bb959c8b58f15cf143
nanoo-n <88988610+nanoo-n@users.noreply.github.com>
2025-01-24 13:03:59 -0300

    complete getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,19 @@
-const getTheTitles = function() {
+//REMOVE BELOW
 
+//REMOVE ABOVE
+
+const getTheTitles = function(input) {
+    let bookTitles = [];
+    for (let i = 0; i < input.length; i++) {
+        bookTitles.push(input[i].title);
+        //console.log(bookTitles);
+    }
+    return(bookTitles);
 };
+
+//REMOVE BELOW
+//getTheTitles();
+//REMOVE ABOVE
 
 // Do not edit below this line
 module.exports = getTheTitles;
