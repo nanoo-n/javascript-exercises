164ab473ef15def2aec3133bbcf1180ac6cc973b
nanoo-n <88988610+nanoo-n@users.noreply.github.com>
2025-02-21 22:28:15 -0300

    finished exercise 12, worked with solution.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,32 @@
-const findTheOldest = function() {
-
+function getAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
+const findTheOldest = function (people) {
+    return people.reduce((oldest, currentPerson) => {
+      const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+      const currentAge = getAge(
+        currentPerson.yearOfBirth,
+        currentPerson.yearOfDeath
+      );
+      return oldestAge < currentAge ? currentPerson : oldest;
+    });
+  };
+
+// const findTheOldest = function(people) {
+//     let total = 0;
+//     let oldestPerson = {};
+//     people.reduce((prev, curr) => {
+//         if(getAge(curr.yearOfBirth, curr.yearOfDeath) > total) {
+//             total = (curr.yearOfBirth, curr.yearOfDeath);
+//             oldestPerson = curr;
+//         }
+//         return prev;
+//     });
+//     return oldestPerson;
+// };
 // Do not edit below this line
 module.exports = findTheOldest;
